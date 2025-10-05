4641c784cf6ca2528064ca9cf05c4517a2563410
TonyGoncharov <potteromics@gmail.com>
2025-10-05 23:34:31 +0300

    Move constants

--- modules/dna_rna_tools.py
+++ modules/dna_rna_tools.py
@@ -1,3 +1,39 @@
+TRANSCR_RULES = {
+    "A": "A",
+    "T": "U",
+    "U": "U",  # in case of virus RNA(-) -> RNA(+) transcription
+    "G": "G",
+    "C": "C",
+    "a": "a",
+    "t": "u",
+    "u": "u",  # in case of virus RNA(-) -> RNA(+) transcription
+    "g": "g",
+    "c": "c",
+}
+
+DNA_COMPL_RULES = {
+    "A": "T",
+    "T": "A",
+    "G": "C",
+    "C": "G",
+    "a": "t",
+    "t": "a",
+    "g": "c",
+    "c": "g",
+}
+
+RNA_COMPL_RULES = {
+    "A": "U",
+    "U": "A",
+    "G": "C",
+    "C": "G",
+    "a": "u",
+    "u": "a",
+    "g": "c",
+    "c": "g",
+}
+
+
 def is_nucleic_acid(sequence) -> bool:
     """
     Checks if the input sequence is DNA or RNA.
--- pytteromics.py
+++ pytteromics.py
@@ -1,37 +1,5 @@
-TRANSCR_RULES = {
-    "A": "A",
-    "T": "U",
-    "U": "U",  # in case of virus RNA(-) -> RNA(+) transcription
-    "G": "G",
-    "C": "C",
-    "a": "a",
-    "t": "u",
-    "u": "u",  # in case of virus RNA(-) -> RNA(+) transcription
-    "g": "g",
-    "c": "c",
-}
-
-DNA_COMPL_RULES = {
-    "A": "T",
-    "T": "A",
-    "G": "C",
-    "C": "G",
-    "a": "t",
-    "t": "a",
-    "g": "c",
-    "c": "g",
-}
-
-RNA_COMPL_RULES = {
-    "A": "U",
-    "U": "A",
-    "G": "C",
-    "C": "G",
-    "a": "u",
-    "u": "a",
-    "g": "c",
-    "c": "g",
-}
+from modules.dna_rna_tools import is_nucleic_acid
+from modules.filter_fastq import gc_content, qscore
 
 
 def filter_fastq(seqs, gc_bounds = (0, 100), length_bounds = (0, 2**32), quality_threshold = 0):
