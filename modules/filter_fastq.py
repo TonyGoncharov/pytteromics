45e308ac926de34eabe69de3d2eedacc39fdd2be
TonyGoncharov <potteromics@gmail.com>
2025-10-05 22:28:28 +0300

    Add "qscore" function

--- modules/filter_fastq.py
+++ modules/filter_fastq.py
@@ -19,3 +19,21 @@ def gc_content(sequence) -> float:
 
     gc_percent = ((g_count + c_count) / sequence_length) * 100
     return gc_percent
+
+
+def qscore(quality_str) -> float:
+    """
+    Calculates the mean phred33 quality score for a given quality string.
+
+    Arguments:
+    quality_str: str — ASCII-encoded quality string
+
+    Returns:
+    float — mean Q-score across all bases.
+    Returns 0.0 for empty strings.
+    """
+    str_length = len(str(quality_str))
+    if str_length == 0:
+        return 0.0
+    q_scores = [ord(symbol) - 33 for symbol in quality_str]
+    return sum(q_scores) / str_length
