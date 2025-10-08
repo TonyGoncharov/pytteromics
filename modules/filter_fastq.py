53f8a7d02151791cd275a568057f1c7bae31cbbf
TonyGoncharov <potteromics@gmail.com>
2025-10-09 00:19:15 +0300

    Update "calc_gc_percent" function

--- modules/filter_fastq.py
+++ modules/filter_fastq.py
@@ -1,27 +1,14 @@
 from modules.dna_rna_tools import is_nucleic_acid
 
 
-def gc_content(sequence: str) -> float:
-    """
-    Calculates GC content for a nucleotide sequence.
-
-    Arguments:
-    sequence: str — input DNA or RNA sequence
-
-    Returns:
-    float — GC percentage (0–100). 
-    Returns 0 for empty sequences.
-    """
+def calc_gc_percent(sequence: str) -> float:
     sequence = str(sequence).upper()
-    sequence_length = len(sequence)
-    if sequence_length == 0:
-        return 0
-    
+    sequence_len = len(sequence)
+    if sequence_len == 0:
+        return 0.0
     g_count = sequence.count("G")
     c_count = sequence.count("C")
-
-    gc_percent = ((g_count + c_count) / sequence_length) * 100
-    return gc_percent
+    return ((g_count + c_count) / sequence_len) * 100
 
 
 def qscore(quality_str: str) -> float:
