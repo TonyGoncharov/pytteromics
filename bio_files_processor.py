7eb1de7dc4368aca83d983afa1e62531cccaa7db
TonyGoncharov <potteromics@gmail.com>
2025-10-12 19:44:57 +0300

    Create `parse_blast_output` function

--- bio_files_processor.py
+++ bio_files_processor.py
@@ -16,3 +16,20 @@ def convert_multiline_fasta_to_oneline(
             while not line.startswith(">"):
                 output_fasta.write(line.strip())
 
+
+def parse_blast_output(
+        input_file: str = 'data/example_blast_results.txt', 
+        output_file: str = 'data/parsed_blast_results.txt'
+) -> str:
+    os.makedirs(os.path.dirname(output_file), exist_ok = True)
+    with open(input_file) as input_file:
+        proteins = []
+        for line in input_file:
+            if line.startswith("Description"):
+                protein = input_file.readline().split("  ")[0]
+                proteins.append(protein)
+    proteins.sort()
+    with open(output_file, "w") as output_file:
+        for protein in proteins:
+            output_file.write(protein + "\n")
+            
