134700c539e2b68b3b39b837cd8c0e38e650439d
TonyGoncharov <potteromics@gmail.com>
2025-10-12 17:50:34 +0300

    Update `filter_fastq` function
    The function now works with real `.fastq` files.

--- pytteromics.py
+++ pytteromics.py
@@ -1,22 +1,32 @@
-from modules.filter_fastq import is_len_passing, is_gc_passing, is_qscore_passing
+from modules.filter_fastq import is_len_passing, is_gc_passing, is_qscore_passing, validate_input, validate_output
 from modules.dna_rna_tools import is_nucleic_acid, prepare_sequences, prepare_tool, transcribe, reverse, complement, reverse_complement
 
 
 def filter_fastq(
-    seqs: dict[str, tuple[str, str]],
+    input_fastq: str,
+    output_fastq: str = "output_fastq.fastq",
     gc_bounds: float | tuple[float, float] = (0, 100),
     length_bounds: int | tuple[int, int] = (0, 2**32),
-    quality_threshold: int | float = 0
-) -> dict[str, tuple[str, str]]: 
-    filtered_seqs = {}
-    for name, (sequence, quality) in seqs.items():
-        if (
-            is_len_passing(sequence, length_bounds)
-            and is_gc_passing(sequence, gc_bounds)
-            and is_qscore_passing(quality, quality_threshold)
-        ):
-            filtered_seqs[name] = (sequence, quality)
-    return filtered_seqs
+    quality_threshold: int | float = 0,
+    output_mode: str = "append"
+) -> str:
+    input_fastq = validate_input(input_fastq)
+    output_fastq, output_mode = validate_output(output_fastq, output_mode)
+    with open(input_fastq) as input_fastq, open(output_fastq, output_mode) as output_fastq:
+        while True:
+            header = input_fastq.readline()
+            if header == "":
+                break
+            sequence = input_fastq.readline()
+            comment = input_fastq.readline()
+            quality = input_fastq.readline()
+
+            if (
+                is_len_passing(sequence, length_bounds)
+                and is_gc_passing(sequence, gc_bounds)
+                and is_qscore_passing(quality, quality_threshold)
+            ):
+                output_fastq.write(header + sequence + comment + quality)
 
 
 def run_dna_rna_tools(*args: str) -> str | bool | list[str | bool]:
